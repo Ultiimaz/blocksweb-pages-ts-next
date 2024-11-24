3ff89c97e1366614e7f637d0482e06d100a78dc8
Ultiimaz <Sven-tjeerd-sma@hotmail.com>
2024-11-24 23:39:39 +0100

    Add fallback message and navigation button for empty dynamic pages

--- src/pages/[[...slug]].tsx
+++ src/pages/[[...slug]].tsx
@@ -9,7 +9,24 @@ import { GetServerPages } from "@blocksweb/core";
 const DynamicPage = (props: {
   page: IBlockswebPage;
   editorComponents: IBlockswebComponent[];
-}) => <DynamicRenderer page={props.page} editorComponents={editorComponents} />;
+}) => {
+  if (!props.page) {
+    return (
+      <div>
+        It seems no pages have been created yet, please create a page in the
+        Blocksweb admin panel.
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => (window.location.href = "/admin/sign-in")}
+        >
+          Go to admin panel
+        </button>
+      </div>
+    );
+  }
+
+  <DynamicRenderer page={props.page} editorComponents={editorComponents} />;
+};
 
 export const getServerSideProps = async (context) => {
   const result = await GetServerPages(context);
