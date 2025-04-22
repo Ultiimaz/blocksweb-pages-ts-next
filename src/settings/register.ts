7bcf375287c392ece4f5e455fe1705efa636dcf0
Ultiimaz <Sven-tjeerd-sma@hotmail.com>
2025-04-23 01:19:31 +0200

    refactor: improve type safety and add assertions in component resolution

--- src/settings/register.ts
+++ src/settings/register.ts
@@ -1,10 +1,6 @@
 import VideoHeroSection from "./components/video-hero-section";
-import ImageLeftTextRightBlock from "./components/image-left-text-right";
 import { IBlockswebComponent } from "@blocksweb/core";
-export const editorComponents: IBlockswebComponent[] = [
-  VideoHeroSection,
-  ImageLeftTextRightBlock,
-];
+export const editorComponents: IBlockswebComponent[] = [VideoHeroSection];
 
 export const settings = {
   editorComponents: editorComponents,
